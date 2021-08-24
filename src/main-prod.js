088686b2ff602c473366de776bd37f07f12d008c
wuliqin <2426631242@qq.com>
2021-08-25 01:44:28 +0800

    优化完成

---
+++ src/main-prod.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+
+import './assets/global.css'
+
+// 引入时间过滤器
+import './plugins/timerInit'
+// 商品分类表格工具
+import treeTable from 'vue-table-with-tree-grid'
+
+// 导入文本输入框
+import VueQuillEditor from 'vue-quill-editor'
+
+// 导入进度条包 js 和css
+import NProgress from 'nprogress'
+
+// 全局配置axios 直接使用 this 可调用到
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+// 请求拦截器 通过axios拦截器添加token验证 中显示进度条
+axios.interceptors.request.use((config) => {
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+// 响应拦截器 response 中隐藏进度条
+axios.interceptors.response.use((config) => {
+  NProgress.done()
+  return config
+})
+Vue.prototype.$axios = axios
+
+Vue.config.productionTip = false
+Vue.component('tree-table', treeTable)
+Vue.use(VueQuillEditor) // 文本编辑器注册使用
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
